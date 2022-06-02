621c3bef4f95f8bebd4f015a5bec63e6568b33bc
Spyke09 <romanov.alexanderromanov@yandex.ru>
2022-06-02 10:53:55 +0300

    fixed task 4

--- task_4/7.py
+++ task_4/7.py
@@ -1,8 +1,8 @@
 from math import *
 
-ro = (lambda x: sqrt(1 - x ** 2))
-xk = (lambda k, n: cos(k * pi / n))
-ak = (lambda k, n: pi / (n+1) * (sin(k*pi/(n+1)))**2)
+ro = (lambda x: sqrt((1 - x)/(1 + x)))
+xk = (lambda k, n: cos(2*k * pi / (2*n+1)))
+ak = (lambda k, n: 4*pi / (2*n+1) * (sin(k*pi/(2*n+1)))**2)
 
 
 def integrate(f, n):
@@ -16,8 +16,9 @@ f1 = lambda x: sin(x) ** 2
 f2 = lambda x: exp(-x ** 2)
 f3 = lambda x: log(x ** 2 + 10) + cos(4.2 * x)
 
-m = 1000
+m = 10
 print("f1: ", integrate(f1, m))
 print("f2: ", integrate(f2, m+1))
 print("f3: ", integrate(f3, m+2))
+
 
